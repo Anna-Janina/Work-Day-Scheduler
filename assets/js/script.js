d8e8fbfd82f35d9ea7acc803e0512f0ae1a07649
Anna <annajaninaschmidt@gmail.com>
2022-12-30 15:57:57 +1100

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var timeDisplayEl = $('#currentDay')
+// handle displaying the time
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+setInterval(displayTime, 1000);
+
+function pastPresentFuture() {
+  // Get the current hour
+  const currentHour = new Date().getHours();
+  // Get all of the time-block div
+  const timeBlocks = document.querySelectorAll('.time-block');
+  // Loop through the time-block div
+  timeBlocks.forEach(timeBlock => {
+    // Get the hour indicated in the id attribute
+    const idHour = parseInt(timeBlock.id.split('-')[1]);
+    // Compare the id hour to the current hour
+    if (idHour < currentHour) {
+      // If the id hour is less than the current hour, add the "past" class
+      timeBlock.classList.add('past');
+    } else if (idHour === currentHour) {
+      // If the id hour is equal to the current hour, add the "present" class
+      timeBlock.classList.add('present');
+    } else {
+      // If the id hour is greater than the current hour, add the "future" class
+      timeBlock.classList.add('future');
+    }
+  });
+}
+window.onload = function() {
+  pastPresentFuture();
+};
+
+//Save data to local storage
+$(".saveBtn").click(function (event) {
+  event.preventDefault();
+  var value = $(this).siblings(".time-block").val();
+  var time = $(this).parent().attr("id").split("-")[1];
+  localStorage.setItem(time,value);
 });
+
+// Get item from local storage if any
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
